3eddd4f850d1a7a9ba6e27fbc994df05c377be98
kakaiking <kakaiphil@gmail.com>
2024-08-25 17:40:10 +0300

    4th commit

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,27 +1,61 @@
-import React, { useState } from 'react'
-import "../styles/Login.scss"
+import React, { useState } from 'react';
+import "../styles/Login.scss";
+import { setLogin } from '../redux/state';
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom"
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const response = await fetch ("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email, password})
+      })
+
+      // Get data after fetching
+      const loggedIn = await response.json()
+
+      if(loggedIn) {
+        dispatch (
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate("/")
+      }
+    } catch(error) {
+      console.log("Login failed", error.message)
+    }
+  }
 
   return (
     <div className='login'>
       <div className="login_content">
-        <form className="login_content_form">
+        <form className="login_content_form" onSubmit={handleSubmit}>
           <input 
             type="email" 
             placeholder='Email' 
             value={email} 
-            onChange={(e) => e.target.value}
+            onChange={(e) => setEmail(e.target.value)}
             required 
           />
           <input 
             type="password" 
             placeholder='Password' 
             value={password} 
-            onChange={(e) => e.target.value}
+            onChange={(e) => setPassword(e.target.value)}
             required 
           />
           <button type='submit'>LOG IN</button>
@@ -30,7 +64,7 @@ const LoginPage = () => {
       </div>
 
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
